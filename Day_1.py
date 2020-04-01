46e69a128f99164419bf0957fc5caa91388696b1
ktrip96 <ktrip96@gmail.com>
2020-04-02 02:25:20 +0300

    Day1

--- Day_1.py
+++
@@ -1,9 +0,0 @@
-def singleNumber(nums):
-    a = 0
-    for i in nums:
-        a ^= i
-    return a
-
-
-b = [int(x) for x in input().split()]
-print(singleNumber(b))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Leetcode_30DaysCodeChallenge
-This is a 30 Day Code Challenge from Leetcode. Each day , different problem. 
+# Week 1
+
