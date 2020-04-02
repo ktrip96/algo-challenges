46fea9e3832c382ae096c7c8d803d6d9072b330d
ktrip96 <ktrip96@gmail.com>
2020-04-03 02:15:36 +0300

    Day_2

---
+++ Day_2/Day_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>       
+
+using namespace std;
+int A[1000], res;
+int sum_square(long long int k){
+    if (k/10 == 0) return pow(k,2);
+    else{
+    return(pow(k%10,2)+ sum_square(k/10));
+    }
+}
+
+bool isHappy(long int n) {
+        res = sum_square(n);
+        if (res == 100 || res == 1000 || res == 10 || res == 1) return true;
+        else if(A[res]==res) return false;
+        else{
+        A[res] = res;
+        return isHappy(res);
+        }
+    }
+
+int main(){
+    long long int i;
+    cin >> i;
+    if(isHappy(i)) cout << "Happy Number"<<endl;
+    else cout << "Non Happy Number"<<endl;
+}
