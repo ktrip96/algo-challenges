31d3e0e9d8a3e745ad3844c5d682d5b01bdddc2f
ktrip96 <ktrip96@gmail.com>
2020-05-11 12:43:34 +0300

    Find the Judge Problem

---
+++ Find_The_Town_Judge/Find.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        map<int,int> M;
+        int judge = -1;
+        if (N == 1) return 1;
+        for (int i = 0; i < trust.size(); i ++){
+            M[trust[i][1]]++;
+            if(M[trust[i][1]] == N - 1) judge = trust[i][1];
+        }
+        for(int i = 0; i < trust.size(); i++){
+            if (trust[i][0] == judge){
+                judge = -1;
+                break;
+            }
+        }
+        return judge;
+        
+    }
+};
