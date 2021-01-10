4f42e14b3eaea8d40c60af6a106825e72f0953d2
ktrip96 <ktrip96@gmail.com>
2021-01-10 06:37:46 +0200

    Ready Memo Fib

---
+++ Dynamic_Programming_JS/fibonacci.js
@@ -0,0 +1,12 @@
+// ** Fibonacci Memoization ** 
+
+const fib = (n, memo = []) => {
+    if(memo[n] !== undefined) return memo[n]
+    if (n <= 2) return 1
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
+}
+
+console.log(fib(3))
+console.log(fib(5))
+console.log(fib(50))
