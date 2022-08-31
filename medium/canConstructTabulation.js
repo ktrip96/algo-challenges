8969201be2a400d7afb0f26fffcbe9ff5a6f3719
ktrip96 <ktrip96@gmail.com>
2022-08-31 16:09:14 +0300

    canConstruct progress

--- medium/canConstructTabulation.js
+++ medium/canConstructTabulation.js
@@ -6,39 +6,23 @@
  * You may reuse elements of wordBank as many times as needed.
  */
 
-const canConstruct = (target, wordBank, memo = {}) => {}
+const canConstruct = (target, wordBank) => {}
 
-function isPrefix(string_a, string_b) {
-  // returns true if the first string_a, is prefix of the string_b
-  for (let i = 0; i < string_a.length; i++) {
-    if (string_a[i] !== string_b[i]) return false
-  }
-
-  return true
-}
-
-function removeString(string_a, string_b) {
-  let newString = ''
-  for (let i = string_a.length; i < string_b.length; i++) {
-    newString += string_b[i]
-  }
-  return newString
-}
-
-console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) // true
-console.log(
-  canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])
-) // false
-console.log(
-  canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
-    'e',
-    'ee',
-    'eee',
-    'eeee',
-    'eeeeee',
-    'eeeeeeee',
-    'eeeeeeeeeeee',
-    'eeeeeeeeeeeeeeeeee',
-  ])
-)
+// console.log(canConstruct('abd'))
+console.log(canConstruct('abcdef', ['ab', 'abc', 'def'])) // true
+// console.log(
+//   canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])
+// ) // false
+// console.log(
+//   canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+//     'e',
+//     'ee',
+//     'eee',
+//     'eeee',
+//     'eeeeee',
+//     'eeeeeeee',
+//     'eeeeeeeeeeee',
+//     'eeeeeeeeeeeeeeeeee',
+//   ])
+// )
 // false
