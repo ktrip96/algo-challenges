672c555ea0e4200d769fd3dfd35b74e349a58438
ktrip96 <ktrip96@gmail.com>
2022-12-03 00:47:41 +0200

    advent of code day one

---
+++ adventOfCode/dayOne/calorieCounting.js
@@ -0,0 +1,30 @@
+// --- Day 1: Calorie Counting ---
+
+const fs = require('fs')
+
+try {
+  const data = fs.readFileSync(
+    '/home/ktrip96/Algorithms/adventOfCode/dayOne/input.txt',
+    'utf8'
+  )
+
+  const arrayData = data.split('\n')
+  let max = 0
+  let tempCounter = 0
+  const totalSnackArray = []
+  for (let i = 0; i < arrayData.length; i++) {
+    if (arrayData[i] === '') {
+      totalSnackArray.push(tempCounter)
+      tempCounter = 0
+    } else {
+      tempCounter = tempCounter + parseInt(arrayData[i])
+    }
+  }
+
+  totalSnackArray.push(tempCounter)
+  totalSnackArray.sort((a, b) => b - a)
+
+  console.log(totalSnackArray[0] + totalSnackArray[1] + totalSnackArray[2])
+} catch (err) {
+  console.error(err)
+}
