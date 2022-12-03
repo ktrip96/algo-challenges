692c56197d1c7a0c9844b2dd871a50a32e7cdb0b
ktrip96 <ktrip96@gmail.com>
2022-12-03 12:24:18 +0200

    day 03 advent of code

--- adventOfCode/day03/Rucksack.js
+++ adventOfCode/day03/Rucksack.js
@@ -19,11 +19,64 @@ try {
     'utf8'
   )
 
-  const gameArray = data.split('\n')
-  // .map((i) => i.split(' '))
-  // .map((singleGame) => calculatePoints(singleGame))
+  // 'a'.charCodeAt(0)
+  // 97
+  // 'A'.charCodeAt(0)
+  // 65
+  const priority = (character) => {
+    console.log({ character })
+    // a - z ==> 1 - 26
+    // A - Z ==> 27 - 62
 
-  console.log(gameArray)
+    // if you are lowercase
+    if (character === character.toLowerCase())
+      return character.charCodeAt(0) - 96
+    // else
+    return character.charCodeAt(0) - 38
+  }
+
+  const calculatePoints = (array) => {
+    const firstLetterHash = {}
+    const secondLetterHash = {}
+    const firstString = array[0]
+    const secondString = array[1]
+    const thirdString = array[2]
+
+    let commonChar = ''
+    for (let i = 0; i < firstString.length; i++) {
+      const char = firstString[i]
+      firstLetterHash[char] = true
+    }
+    for (let i = 0; i < secondString.length; i++) {
+      const char = secondString[i]
+      secondLetterHash[char] = true
+    }
+    for (let i = 0; i < thirdString.length; i++) {
+      const char = thirdString[i]
+      if (firstLetterHash[char] && secondLetterHash[char])
+        commonChar = thirdString[i]
+    }
+
+    return priority(commonChar)
+  }
+
+  const gameArray = data.split('\n').map((l) => l.split(','))
+  let finalGameArray = []
+  let i = 0
+  let tempArray = []
+  while (i + 2 <= gameArray.length) {
+    tempArray = gameArray[i].concat(gameArray[i + 1]).concat(gameArray[i + 2])
+    finalGameArray.push(tempArray)
+    tempArray = []
+    i = i + 3
+  }
+
+  const result = finalGameArray
+    .map((l) => calculatePoints(l))
+    .reduce((a, b) => a + b)
+  console.log(result)
+  // .map((line) => calculatePoints(line))
+  // .reduce((a, b) => a + b)
 } catch (err) {
   console.error(err)
 }
